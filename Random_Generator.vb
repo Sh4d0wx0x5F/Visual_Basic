1b38afc246d78caf314cd877869543611c36f7a4
Muhammad Arafat <97059953+Arafat05nov@users.noreply.github.com>
2023-02-15 23:56:52 +0500

    Update Random_Generator.vb

--- Random_Generator.vb
+++ Random_Generator.vb
@@ -2,15 +2,20 @@ Module Module1
 
     Sub Main()
         Dim RNum(99) As Integer
-        Dim count, x As Integer
+        Dim count, x, num As Integer
 
         count = 0
         For x = 0 To 99
             RNum(x) = Int((200 * Rnd()) + 1)
-            count = count + 1
+
+            If RNum(x) >= 66 And RNum(x) <= 173 Then
+                count = count + 1
+            End If
         Next
+
         Console.WriteLine("The number of random numbers generated between 66 and 173 are:" & count)
         Console.ReadKey()
     End Sub
 
 End Module
+
